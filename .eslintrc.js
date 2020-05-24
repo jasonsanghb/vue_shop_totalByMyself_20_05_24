d20d4a5d9b8a90940a72032fee6c18a35b3ac08a
jasonsanghb <670035399@qq.com>
2020-05-24 20:05:45 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,33 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': 0,
+    'object-curly-spacing': [0, 'never'],
+    'eol-last': 0,
+    "no-trailing-spaces": 1,
+    "import/no-duplicates":0,
+    "no-multiple-empty-lines": [1, {"max": 2}],
+    "spaced-comment": 0,
+    "space-before-function-paren": [0, "always"],
+    "space-before-blocks": [0, "always"],
+    'key-spacing':0,
+    "padded-blocks": 0,
+    "semi": [1, "always"],
+    "arrow-spacing": 0,
+    "keyword-spacing":1,
+    "semi-spacing": [0, {"before": false, "after": true}],
+    "quotes": [1, "single"],
+    "comma-spacing": 0,
+    "no-multi-spaces": 1,
+    "quote-props":[1, "always"],
+    "no-useless-return":1,
+    "space-infix-ops": 0,
+    "vue/no-unused-vars":1,
+    "no-unused-vars":1,
+    "camelcase": 1,
+    "space-in-parens": [0, "never"]
   }
+
 }
---
+++ src/components/Login.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="container">
+        <div class="login-wrap">
+            <div class="login-icon">
+                <img src="../assets/logo.png" alt="logo">
+            </div>
+            <el-form :model="loginForm" :rules="loginFormRules" ref="loginFormRef" class="formWrap">
+                <el-form-item class="form-item" prop="username">
+                    <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
+                </el-form-item>
+                <el-form-item class="form-item" prop="password">
+                    <el-input type="password" prefix-icon="iconfont icon-3702mima" v-model="loginForm.password"></el-input>
+                </el-form-item>
+                <el-form-item class="form-item last-item">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+
+            </el-form>
+
+        </div>
+    </div>
+</template>
+<script>
+
+    export default {
+        data() {
+            return {
+                loginForm:{
+                    username:'admin',
+                    password:'123456'
+                },
+                loginFormRules:{
+                    username:[
+                        { required: true , message : '请输入用户名称' , trigger:'blur' },
+                        { min:3 , max:15, message : '请输入3-15个字符之间的用户名称' , trigger:'blur' }
+
+                    ],
+                    password:[
+                        { required: true , message : '请输入用户密码' , trigger:'blur' },
+                        { min:6 , max:15, message : '请输入6-15个字符之间的密码' , trigger:'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            resetLoginForm(){
+                this.$refs.loginFormRef.resetFields()
+            },
+            login(){
+                this.$refs.loginFormRef.validate(async valid=>{
+                    if(!valid){
+                        return
+                    }
+                    const {data:res} = await this.$http.post('login',this.loginForm)
+                    if(res.meta.status!==200){
+
+                        return this.$message.error('登录失败')
+                    }
+                    this.$message.success('登录成功')
+                    this.resetLoginForm()
+                    this.$router.push('/home')
+                })
+            }
+        }
+    };
+</script>
+<style lang="less" scoped>
+
+    .container {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        background-color: #2B4B6B;
+    }
+    .login-wrap{
+        position: absolute;
+        top:50%;
+        left:50%;
+        transform: translate(-50%,-50%);
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+    }
+    .login-icon{
+        position: absolute;
+        top:0;
+        left:50%;
+        transform: translate(-50%,-50%);
+        width: 130px;
+        height: 130px;
+        padding: 10px;
+        border-radius: 50%;
+        background-color: #fff;
+        box-shadow: 0 0 10px #ddd;
+
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+
+    }
+    .formWrap{
+        margin-top: 115px;
+    }
+    .form-item{
+        position: relative;
+        margin: 20px;
+    }
+    .last-item{
+        position: absolute;
+        bottom: 0;
+        right:0;
+    }
+
+</style>
